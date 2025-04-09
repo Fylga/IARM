13f469ab809818871a39ff698a7577989edcc6fe
Fylga <joris.francin@epitech.eu>
2025-04-09 18:40:58 +0200

    feat: linked to azure portal env table

--- api/app/core/database.py
+++ api/app/core/database.py
@@ -2,15 +2,22 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+import logging
+from urllib.parse import quote_plus
+from datetime import datetime
 
-load_dotenv()
+logger = logging.getLogger("app")
+logger.setLevel(logging.INFO)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DATABASE_URL")
+db_name = os.getenv("DB_NAME")
+db_port = os.getenv("DB_PORT")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL n'est pas défini dans .env")
+sql_url = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_user, db_password, db_host, db_port, db_name, "require")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(sql_url, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
